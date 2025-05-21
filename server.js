f28d7309deca71b7eb57dbf2fe6cacdb292d1767
AndreFernandezGenius <aferigl.genius@gmail.com>
2025-05-21 13:43:03 +0200

    segundo commit

--- server.js
+++ server.js
@@ -1,20 +1,46 @@
 const express = require("express");
 const path = require("path");
+const cors = require ('cors')
 const app = express();
 
+
 //app.use (cors());
+app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
+
 // Accedo o arquivo estático
 app.use(express.static(path.join(__dirname, "static")));
 app.post("/acceso",(req,res) => {
       const {nome,email} = req.body; //desestructutra o obxeto de entrada
       console.log(nome,email);
-       if(req.body.nome == 'Andre' && req.body.email == 'andre@gmail.com'){
-        res.sendFile(path.join(__dirname, "static/views/app.html"));
-       }else{
-        res.send("<h1> Usuario no autorizado</h1>");
+      let condicionUsuarioCorrecto= req.body.nome == 'Andre' && req.body.email == 'andre@gmail.com'
+      let datoEnviadoCondicionUsuarioCorrecto ={
+       //if(req.body.nome == 'Andre' && req.body.email == 'andre@gmail.com'){
+        //res.send ({
+           resposta:"acceso autorizado",
+           usuario:{
+            nome: 'Andre'
+           }
+          };
+        //res.sendFile(path.join(__dirname, "static/views/app.html"));
+        let datoEnviadoEnErro = {resposta:"faltan campos ou usuario non rexistrado"}
+
+
+        if (condicionUsuarioCorrecto){
+          res.send(datoEnviadoCondicionUsuarioCorrecto);
+        }else{
+        res.send({resposta:"faltan campos ou usuario no rexistrado"});
        }
-});
+
+       if (condicionUsuarioCorrecto) {
+           res.send(datoEnviadoCorrecto)
+      }else{
+            res.send({usuario:{
+              nome:'Andre'
+            }})
+
+};
 app.get("/enviousuario", (req,res) => {
     res.send({usuario:{
           nome:'Andre'
